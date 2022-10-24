b93f02d14f0b884aab0d47bffc9fb03c32475ba4
AliceAnhCao <112453380+AliceAnhCao@users.noreply.github.com>
2022-10-24 22:44:25 +0700

    Add files via upload

--- app/run.py
+++ app/run.py
@@ -67,6 +67,26 @@ def index():
                 }
             }
         }
+
+        {
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+        }
+        
     ]
     
     # encode plotly graphs in JSON
